c099389b7d404889d2dc07600fff9ab3721dda6f
Dogey11 <69096657+Dogey11@users.noreply.github.com>
2022-01-28 14:55:02 -0800

    v0.5

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 import os
+from os.path import exists
 import shutil
 
-print("\nB3 Installer\nV0.4")
+print("\nB3 Installer\nV0.5")
 print("\nIf you are stuck, view README.md file\n")
 
 # GitHub Copilot is a great thing
 
 input("\nRequired modules will now be installed. Press enter to continue. ")
-os.system("pip install selenium")
-os.system("pip install webdriver-manager")
+os.system("pip install -r Required")
 
-print("Chromedriver will now be installed.")
-input("Press Enter to continue...")
+if exists("C:/Program Files/Google/Chrome/Application/chrome.exe"):
+    pass
+elif exists("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"):
+    pass
+else:
+    print("\nGoogle Chrome was not found. Please make sure it's installed before continuing\n")
+    input("Press enter to continue. ")
+
+os.system("cls")
+print("\nChromedriver will now be installed.")
+input("Press Enter to continue. ")
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -31,10 +40,9 @@ except FileExistsError:
     pass
 
 cwd = os.getcwd()
-shutil.move(cwd + r"\main.py", r"C:\b3\main.py")
-shutil.move(cwd + r"\verify.py", r"C:\b3\verify.py")
-shutil.move(cwd + r"\b3.py", r"C:\b3\b3.py")
-shutil.move(cwd + r"\ico\pyinstallerdefault.ico", r"C:\b3\ico\pyinstallerdefault.ico")
+shutil.move(cwd + r"/main.py", r"C:/b3/main.py")
+shutil.move(cwd + r"/verify.py", r"C:/b3/verify.py")
+shutil.move(cwd + r"/b3.py", r"C:/b3/b3.py")
 
 os.system("cls")
 
--- uninstall.py
+++ uninstall.py
@@ -1,15 +1,11 @@
-import os
 import shutil
-from sys import argv
 
-yn = input(r"Are you sure? This will delete all B3 related files in the C:\b3 directory [y\N] ")
+yn = input(r"Are you sure? This will delete all files in the C:\b3 directory [y\N] ")
 yn = yn.lower()
+
 if yn == "y":
     shutil.rmtree("C:/b3")
 elif yn == "n":
     input("Deletion canceled. Press Enter to close. ")
 else:
     input("Invalid input. Press Enter to close. ")
-
-if yn == "y":
-    os.remove(argv[0])
