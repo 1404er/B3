c417f02671ff7d02716985838f13700c4adadbf2
Dogey11 <69096657+Dogey11@users.noreply.github.com>
2022-03-07 16:51:49 -0800

    0.8

--- setup/B3setup.bat
+++ setup/B3setup.bat
@@ -1,18 +1,22 @@
 @ECHO OFF
 
 ECHO:
-ECHO B3 v0.7 Installer
-ECHO (Installer v0.1)
+ECHO B3 Installer
 ECHO:
 ECHO Required modules will now be installed.
 
 @PAUSE
 
-if exist C:\B3\ (
+CD C:\Windows\Temp
+curl -s https://raw.githubusercontent.com/Dogey11/B3/main/B3req.txt > B3req.txt
+
+pip install -r B3req.txt
+
+IF EXIST C:\B3\ (
     @RD /S /Q C:\B3\
 )
-mkdir C:\B3
-cd C:\B3
+MKDIR C:\B3
+CD C:\B3
 
 curl -s https://raw.githubusercontent.com/Dogey11/B3/main/src/B3.py > B3.py
 curl -s https://raw.githubusercontent.com/Dogey11/B3/main/src/main.py > main.py
@@ -21,11 +25,33 @@ curl -s https://raw.githubusercontent.com/Dogey11/B3/main/src/verify.py > verify
 
 curl -s https://raw.githubusercontent.com/Dogey11/B3/main/LICENSE > LICENSE.txt
 
-cd C:\Windows\Temp
-curl -s https://raw.githubusercontent.com/Dogey11/B3/main/Required > B3req.txt
+CLS
 
-pip install -r B3req.txt
+CHOICE /c ync /n /m "Make executable? (8MB needed) (Y/N): "
+IF %ERRORLEVEL% == 1 (
+    pip uninstall enum34 -y
+    pyinstaller --onefile B3.py
+    DEL B3.py
+    @RD /S /Q C:\B3\__pycache__
+    @RD /S /Q C:\B3\build
+    MOVE C:\B3\dist\B3.exe C:\B3\B3.exe
+    @RD /S /Q C:\B3\dist
+    CLS
+)
+IF %ERRORLEVEL% == 2 (
+    ECHO: 
+    ECHO Skipping... 
+    ECHO:
+)
+IF %ERRORLEVEL% == 3 ( EXIT /B )
 
-cls
-ECHO Install complete. You may now close this window.
+ECHO:
+
+ECHO Install complete. Running verification test...
+ECHO:
+
+verify.py
+
+ECHO:
 @PAUSE
+EXIT /B
