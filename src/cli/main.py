2b3b964dc97602f51cdef22b2f1d92b0bbd22327
Dogey11 <69096657+Dogey11@users.noreply.github.com>
2022-06-29 01:47:12 -0700

    13

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,8 +1,9 @@
-import selenium
-import selenium.common.exceptions as ec
 from time import sleep
 from datetime import datetime
-from selenium import webdriver
+from sys import argv
+import selenium
+import selenium.webdriver
+import selenium.common.exceptions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
@@ -10,49 +11,68 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-link = None
-email = None
-pwd = None
-cvv = None
+FREE_ITEM = "https://www.bestbuy.com/site/free-youtube-premium-for-3-months-new-subscribers-only/6453905.p?skuId=6453905"
 
 
-def askForLink():
-    global link
-    link = input("BestBuy Item Link: ")
-    link = link.lower()
+def tryParse(i):
+    try:
+        int(i)
+        return True
+    except:
+        pass
+    return False
 
 
-def askForInfo():
-    global email
-    global pwd
-    global cvv
+def verifyMode():
+    match len(argv):
+        case 2:
+            return argv[1] == "--verify"
+        case _:
+            return False
 
-    email = input("BestBuy Account Email: ")
-    pwd = input("BestBuy Account Password: ")
 
+def getLink():
+    return input("BestBuy Item Link: ").lower()
+
+
+def getEmail():
+    return input("BestBuy Account Email: ")
+
+
+def getPassword():
+    return input("BestBuy Account Password: ")
+
+
+def getCVV():
     print("\nThis is required by BestBuy to use the default payment.")
+
     cvv = input("BestBuy Default Payment Method CVV/CVC: ")
+    if len(cvv) <= 4 and tryParse(cvv):
+        return cvv
+    else:
+        raise Exception("Invalid.")
 
 
-def time():
+def getTime():
     return datetime.now().strftime("%m/%d/%Y %H:%M:%S")
 
 
-def run():
-    askForLink()
-    askForInfo()
-
-    bought = False
+def main():
+    link = getLink()
+    email = getEmail()
+    pwd = getPassword()
+    cvv = getCVV()
 
     service = Service(executable_path=GeckoDriverManager().install())
 
-    with webdriver.Firefox(service=service) as driver:
+    with selenium.webdriver.Firefox(service=service) as driver:
         driver.get(link)
-        while not bought:
+        while True:
             try:
-                addToCartBtn = WebDriverWait(driver, 3).until(
+                addToCartBtn = WebDriverWait(driver, 3).until
+                (
                     presence_of_element_located((By.CLASS_NAME, 
-                "c-button c-button-disabled c-button-lg c-button-block add-to-cart-button"))
+                    "c-button c-button-disabled c-button-lg c-button-block add-to-cart-button"))
                 )
             except ec.NoSuchElementException:
                 try:
@@ -61,21 +81,24 @@ def run():
                 except ec.NoSuchElementException:
                     pass
 
-                addToCartBtn = WebDriverWait(driver, 10).until(
+                addToCartBtn = WebDriverWait(driver, 10).until
+                (
                     presence_of_element_located((By.CLASS_NAME, 
-                "c-button c-button-primary c-button-lg c-button-block c-button-icon c-button-icon-leading add-to-cart-button"))
+                    "c-button c-button-primary c-button-lg c-button-block c-button-icon c-button-icon-leading add-to-cart-button"))
                 )
 
-                print(f"Available as of {time()}")
+                print(f"Available as of {getTime()}")
                 addToCartBtn.click()
                 driver.get("https://www.bestbuy.com/cart")
 
-                toBuyScreen = WebDriverWait(driver, 60).until(
+                toBuyScreen = WebDriverWait(driver, 60).until
+                (
                     presence_of_element_located((By.XPATH, "//*[text()='Checkout']"))
                 )
                 toBuyScreen.click()
 
-                enterEmail = WebDriverWait(driver, 60).until(
+                enterEmail = WebDriverWait(driver, 60).until
+                (
                     presence_of_element_located((By.ID, "fld-e"))
                 )
 
@@ -87,7 +110,8 @@ def run():
                 signInBtn = driver.find_element(By.XPATH, "//*[text()='Sign In']")
                 signInBtn.click()
 
-                enterCVV = WebDriverWait(driver, 600).until(
+                enterCVV = WebDriverWait(driver, 600).until
+                (
                     presence_of_element_located((By.ID, "cvv"))
                 )
 
@@ -96,16 +120,16 @@ def run():
                 confirmBuy = driver.find_element(By.CLASS_NAME, "btn-lg.btn-block.btn-primary.button__fast-track")
                 confirmBuy.click()
                 sleep(5)
-                print(f"Purchased at {time()}")
-                bought = True
+                print(f"Purchased at {getTime()}")
+                break
             except selenium.common.exceptions.TimeoutException:
-                print(f"Out of Stock as of {time()}")
+                print(f"Out of Stock as of {getTime()}")
                 driver.refresh()
 
-        sleep(60)
-        driver.quit()
-    input(f"Finished at {time()}")
+    sleep(60)
+    print(f"Finished at {getTime()}")
+    driver.quit()
 
 
 if __name__ == "__main__":
-    run()
+    main()
